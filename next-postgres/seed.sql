8f6ae41bf9324aabdad29e9cc8315d27002356d2
HANNAH <HUPEE2001@YAHOO.COM>
2024-05-09 10:38:12 +0100

    .

---
+++ next-postgres/seed.sql
@@ -0,0 +1,11 @@
+CREATE TABLE froguins (
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    description TEXT
+);
+
+INSERT INTO froguins (name, description) VALUES
+('Edwina', 'Very good girl our Edwina is.'),
+('Tarquin', 'Tarquin is probably a baddy. We haven&apos;t figured it out yet'),
+('Targuin', 'Targuin is probably going to defeat Tarquin one day. We haven&apos;t figured it out yet'),
+('Pingu', 'Pingu is a maniac. He taught kids how to be naughty.');
